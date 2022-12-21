f91761147e6dd2750a56a5d638197144239f6b66
amorenog9 <alejandro.moreno.garcia@alumnos.upm.es>
2022-12-21 11:15:39 +0100

    Minor changes

--- src/main/scala/es/upm/dit/SparkReaderTable2.scala
+++ src/main/scala/es/upm/dit/SparkReaderTable2.scala
@@ -54,34 +54,49 @@ object SparkReaderTable2{
     //Ejecutamos el script que realiza el stream de los eventos posteriores al timestamp almacenados en la BBDD
     s"python3 ${pythonScriptFile}".! //con ! bloqueamos hasta que termine de enviarse lo del script; con run se paraleliza https://www.scala-lang.org/files/archive/api/current/scala/sys/process/ProcessBuilder.html
 
-/*
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val path = savePathEventsFromTimestamp + s"/${timeStampValue}" + "/eventsFromTimestamp.json"
-    val inputFormat = new TextInputFormat(new Path(path));
-    inputFormat.setCharsetName("UTF-8");
-    val ds = env.readFile(inputFormat, path)
+      val env = StreamExecutionEnvironment.getExecutionEnvironment
+      val kafkaProperties = new Properties()
+      kafkaProperties.setProperty("bootstrap.servers", parametros.getString("KAFKA_DIRECTION_IN"))
+      kafkaProperties.setProperty("group.id", "test")
+
+      val kafkaConsumer = new FlinkKafkaConsumer(
+        KAFKA_TOPIC_IN,
+        new JsonNodeDeserializationSchema(), //deserializes a JSON String into an ObjectNode.
+        kafkaProperties) //.setStartFromEarliest()
+
+      val train: DataStream[TrainEvent] = env
+        .addSource(kafkaConsumer) //.setStartFromLatest())
+        .map(jsonNode => TrainEvent(
+          id = jsonNode.get(s"${id}").asText(),
+          event_type = jsonNode.get(s"${event_type}").asText(),
+          date_event = jsonNode.get(s"${date_event}").asLong(), // fecha en epoch milliseconds
+          lat = jsonNode.get(s"${lat}").asDouble(),
+          lng = jsonNode.get(s"${lng}").asDouble(),
+          location = jsonNode.get(s"${location}").asText()
+        ))
+
+      val KEY = train.keyBy(_.id)
+        .flatMap(new EventProcessorFromTimestamp())
 
 
-    val keyedListTrains = ds
-      .flatMap(new EventProcessorFromTimestamp())
+      // Sinks
+      KEY.print()
+
+      KEY
+        .map(_.asJson.noSpaces)
+        .addSink(new FlinkKafkaProducer[String](
+          parametros.getString("KAFKA_DIRECTION_OUT"),
+          KAFKA_TOPIC_OUT,
+          new SimpleStringSchema))
+
+
+      env.execute("FLink-Execution")
 
 
 
-    ds.print()
 
 
-
-
-
-    env.execute("jobname02");
-
-
-
-
-    println(json)
-
- */
-
+    println("HE ACABADOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
   }
 
 
