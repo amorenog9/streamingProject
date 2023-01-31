e045fef18b5eec1a8eef0b32b88a6c2d383e204a
amorenog9 <alejandro.moreno.garcia@alumnos.upm.es>
2023-01-31 12:45:23 +0100

    Tarea 3), añadido comentarios

---
+++ src/main/scala/es/upm/dit/CleanKafkaTopics.scala
@@ -0,0 +1,40 @@
+package es.upm.dit
+
+import com.typesafe.config.ConfigFactory
+
+import sys.process._
+
+
+
+/*
+  * Herramienta auxiliar para resetear todos los kafkaTopics de manera rápida sin tener que
+  * volver a desplegar todo docker
+
+ */
+
+object CleanKafkaTopics{
+  def main(args: Array[String]) {
+
+    // Config
+    val parametros = ConfigFactory.load("applicationTrain.conf")
+    val kafkaDir = parametros.getString("KAFKA_DIR")
+    val messages_in = "messages_in"
+    val messages_out = "messages_out"
+    val messages_out_no_memory = "messages_out_no_memory"
+    val messages_from_timestamp_out = "messages_from_timestamp_out"
+
+
+
+    s"${kafkaDir}/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic ${messages_in}".!
+    s"${kafkaDir}/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic ${messages_out}".!
+    s"${kafkaDir}/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic ${messages_out_no_memory}".!
+    s"${kafkaDir}/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic ${messages_from_timestamp_out}".!
+
+    s"${kafkaDir}/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ${messages_in}".!
+    s"${kafkaDir}/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ${messages_out}".!
+    s"${kafkaDir}/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ${messages_out_no_memory}".!
+    s"${kafkaDir}/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ${messages_from_timestamp_out}".!
+
+
+  }
+}
